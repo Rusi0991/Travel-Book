1b1f973955ab6b7dab9eebb2f574d05fc744a3af
Rusi0991 <ruslanismayilov484@gmail.com>
2021-09-23 19:21:52 -0700

    Travel Book file created

---
+++ Travel Book/Travel Book/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Travel Book
+//
+//  Created by Ruslan Ismayilov on 9/23/21.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
